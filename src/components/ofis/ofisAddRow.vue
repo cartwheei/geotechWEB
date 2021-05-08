bf26032ee10ba51ba202726ac01be74497fd7161
cartwheei <20185181008@ogr.akdeniz.edu.tr>
2021-05-08 09:40:04 +0300

    veri gönderilebiliyor

--- src/components/ofis/ofisAddRow.vue
+++ src/components/ofis/ofisAddRow.vue
@@ -2,26 +2,65 @@
     <div class="addRow">
         <div class="row">
             <div class="col">
-                <input type="text" class="form-control" aria-label="Medium" aria-describedby="inputGroup-sizing-sm">
+                <input id="il_kod" type="text" class="form-control" aria-label="Medium" 
+                aria-describedby="inputGroup-sizing-sm" v-model="il_kod">
             </div>
             <div class="col">
-                <input type="text" class="form-control" aria-label="Medium" aria-describedby="inputGroup-sizing-sm">
+                <input id="numarataj" type="text" class="form-control" aria-label="Medium" 
+                aria-describedby="inputGroup-sizing-sm" v-model="numarataj">
             </div>
              <div class="col">
-                <input type="text" class="form-control" aria-label="Medium" aria-describedby="inputGroup-sizing-sm">
+                <input id="geometri" type="text" class="form-control" aria-label="Medium" 
+                aria-describedby="inputGroup-sizing-sm" v-model="geometri">
             </div>
             <div class="col">
-                <input type="text" class="form-control" aria-label="Medium" aria-describedby="inputGroup-sizing-sm">
+                <input id="toplamIs" type="text" class="form-control" aria-label="Medium"
+                 aria-describedby="inputGroup-sizing-sm" v-model="toplamIs">
             </div>
             <div class=" col align-self-medium">
-              <button class="btn btn-success">Ekle</button>
+              <button v-on:click="addIs()" class="btn btn-success">Ekle</button>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+    data(){
+        return{
+            il_kod:"",
+            numarataj:"",
+            geometri:"",
+            toplamIs:"",
+        }
+    },
+    methods:{
+        addIs(){
+            const header={
+                'Content-Type': 'application/json;charset=UTF-8',
+            }
+
+            var obj = {
+                "il_kodu":this.il_kod,
+                "numarataj":this.numarataj,
+                "geometri":this.geometri,
+                "toplam_is":this.toplamIs
+            }
+
+            axios.post("http://localhost:2022/ofispost", obj,{
+                headers:header}) 
+                .then((response)=>{
+                console.log(response)
+            }).catch(error => {
+                    if(error.response){
+                        console.log(error.response)
+                        alert('verileri kontrol ediniz')
+                    }
+            })
+        }
+    }
     
 }
 </script>
