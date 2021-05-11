42e42b1e86421b438feef2b198de6e0ffc884369
cartwheei <20185181008@ogr.akdeniz.edu.tr>
2021-05-11 11:07:43 +0300

    map v2

--- src/mapGenel.vue
+++ src/mapGenel.vue
@@ -9,10 +9,18 @@
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { onMounted } from "vue";
+import axios from 'axios'
 
 export default {
+  data(){
+    return{
+      // iller:[],
+      params:{"il_kodlari":[1,2]}
+    }
+  },
   setup() {
     onMounted(() => {
+      console.log(this.iller,'alper')
       mapboxgl.accessToken =
         "pk.eyJ1IjoiY2FydHdoZWVsIiwiYSI6ImNranp4em5rczBjN2Qyb2syOHR2eWhhcGkifQ.JVVl04Dnmq0xIKJiER_C8A   ";
       var map = new mapboxgl.Map({
@@ -20,12 +28,42 @@ export default {
         style: "mapbox://styles/mapbox/light-v9",
       });
       map.on('load', () => {
+        map.addSource('iller2',{
+          'type':'geojson',
+          'data':this.iller[0]
+        })
+
+        map.addLayer({
+          'id': 'maine',
+          'type': 'fill',
+          'source': 'iller2', // reference the data source
+          'layout': {},
+          'paint': {
+          'fill-color': '#0080ff', // blue color fill
+          'fill-opacity': 0.5
+          }
+          });
+
+
       // TODO: Here we want to load a layer
       // TODO: Here we want to load/setup the popup
       });
     });
     return {};
   },
+  created(){
+          axios.post("http://localhost:2022/geo",this.params).then(response=>{
+        
+        var iller = response.data.result.features
+        // this.iller = response.data.result.features
+        console.log(iller,'alper2')
+        }).catch(e=>{
+          console.log(e)
+        })         
+  }
+
+  
+
 };
 </script>
 
