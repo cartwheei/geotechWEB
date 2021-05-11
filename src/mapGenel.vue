0699112a5df28bf55c5bb0f9c28815a89c2e242d
cartwheei <20185181008@ogr.akdeniz.edu.tr>
2021-05-11 12:00:45 +0300

    map v3

--- src/mapGenel.vue
+++ src/mapGenel.vue
@@ -8,29 +8,32 @@
 <script>
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-import { onMounted } from "vue";
+// import { onMounted } from "vue";
 import axios from 'axios'
 
 export default {
   data(){
     return{
-      // iller:[],
-      params:{"il_kodlari":[1,2]}
+      iller:{},
+      params:{"il_kodlari":[26,7,9,16,33,48,54,60,10]}
     }
   },
-  setup() {
-    onMounted(() => {
-      console.log(this.iller,'alper')
+
+    mounted(){
       mapboxgl.accessToken =
         "pk.eyJ1IjoiY2FydHdoZWVsIiwiYSI6ImNranp4em5rczBjN2Qyb2syOHR2eWhhcGkifQ.JVVl04Dnmq0xIKJiER_C8A   ";
       var map = new mapboxgl.Map({
         container: "map",
         style: "mapbox://styles/mapbox/light-v9",
+        zoom:5,
+        center:[33.441933,39.487294]
       });
+      
+
       map.on('load', () => {
         map.addSource('iller2',{
           'type':'geojson',
-          'data':this.iller[0]
+          'data':this.iller
         })
 
         map.addLayer({
@@ -48,15 +51,16 @@ export default {
       // TODO: Here we want to load a layer
       // TODO: Here we want to load/setup the popup
       });
-    });
-    return {};
-  },
+    },
+    
+  
   created(){
-          axios.post("http://localhost:2022/geo",this.params).then(response=>{
+    
+        axios.post("http://localhost:2022/geo",this.params).then(response=>{
         
-        var iller = response.data.result.features
-        // this.iller = response.data.result.features
-        console.log(iller,'alper2')
+        // var iller = response.data.result.features
+        this.iller = response.data.result
+        console.log(this.iller)
         }).catch(e=>{
           console.log(e)
         })         
